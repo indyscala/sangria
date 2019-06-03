b2751be9b9cc1f9b210eea5e23dd2372cd37d4ea
Brad Fritz <brad@fewerhassles.com>
2019-06-02 20:15:45 -0400

    introduce interfaces and the derivation macros

--- src/main/scala/SchemaDefinition.scala
+++ src/main/scala/SchemaDefinition.scala
@@ -1,15 +1,32 @@
 import sangria.schema._
 
+import sangria.macros.derive._
+
 /**
  * GraphQL schema for our demo vehicle API.
  */
 object SchemaDefinition {
-  val CountryType = ObjectType(
+  implicit val CountryType = ObjectType(
     "Country",
     "Country might be where a Make is headquartered or where a vehicle is manufactured.",
 
     fields[Unit, Country](
       Field("name", StringType, resolve = _.value.name),
-    )
+    ),
+  )
+
+  val IdentifiableType = InterfaceType(
+    "Identifiable",
+    "Entity that can be identified",
+
+    fields[Unit, Identifiable](
+      Field("id", StringType, resolve = _.value.id),
+    ),
+  )
+
+  val MakeType = deriveObjectType[Unit, Make](
+    Interfaces(IdentifiableType),
+    ObjectTypeDescription("The vehicle manufacturer"),
+    DocumentField("name", "Name of the manufacturer"),
   )
 }
