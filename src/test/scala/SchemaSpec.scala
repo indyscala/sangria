ff800bfbd471de614e5cc8821061c7181a9b6a6e
Brad Fritz <brad@fewerhassles.com>
2019-06-02 23:59:57 -0400

    add spec to render schema to SDL

--- src/test/scala/SchemaSpec.scala
+++ src/test/scala/SchemaSpec.scala
@@ -1,16 +1,19 @@
 import org.scalatest._
-
 import io.circe.Json
 import sangria.execution._
 import sangria.macros._
 import sangria.marshalling.circe._
+import sangria.renderer.SchemaRenderer
 
-import scala.concurrent.{Await,Future}
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-
 class SchemaSpec extends FlatSpec with Matchers {
+  "schema definition" should "be renderable" in {
+    println(SchemaRenderer.renderSchema(SchemaDefinition.schema))
+  }
+
   "countries query" should "list countries" in {
     val query = graphql"""
       query AllCountries {
